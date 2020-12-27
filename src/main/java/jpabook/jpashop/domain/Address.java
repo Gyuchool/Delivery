5ce10d34ce7c56a06803d6687b7532e1c8535032
Gyuchool <balance0591@naver.com>
2020-12-27 20:30:50 +0900

    Entity

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,14 +1,23 @@
 package jpabook.jpashop.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 
 @Embeddable //내장타입 포함
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
+
+    public Address(String city, String street, String zipcode) {
+        this.city=city;
+        this.street=street;
+        this.zipcode=zipcode;
+    }
 }
